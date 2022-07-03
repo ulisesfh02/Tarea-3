18bca07a40f19fbcd5ad526d01b218417af1f344
ulisesfh02 <jose.fonsecahurtado@ucr.ac.cr>
2022-07-03 16:07:01 -0600

    Nuevos cambios

--- Makefile
+++ Makefile
@@ -1,16 +1,21 @@
 FLAGS = -g -c --std=c++17
 
 all:
+	mkdir -p obj
 	mkdir -p bin
-	g++ $(FLAGS) src/Tienda.cpp -o bin/Tienda.o
-	g++ $(FLAGS) src/Producto.cpp -o bin/Producto.o
-	g++ -g -o bin/calcularEnvios bin/Tienda.o
+	g++ $(FLAGS) src/Tienda.cpp -o obj/Tienda.o
+	g++ $(FLAGS) src/Producto.cpp -o obj/Producto.o
+	g++ -shared -o bin/libtienda.so obj/Tienda.o obj/Producto.o
+	mkdir -p bin/include
+	cp src/Tienda.h  src/Producto.h ./bin/include
+
 test:
+	mkdir -p obj
 	mkdir -p bin
-	g++ $(FLAGS) tests/tests_tienda.cpp -o bin/tests_tienda.o
-	g++ $(FLAGS) src/Tienda.cpp -o bin/Tienda.o
-	g++ $(FLAGS) tests/tests_producto.cpp -o bin/tests_producto.o
-	g++ $(FLAGS) src/Producto.cpp -o bin/Producto.o
-	g++ -g -o bin/tests bin/tests_tienda.o bin/Tienda.o bin/tests_producto.o bin/Producto.o -lgtest -lgtest_main -lpthread
+	g++ $(FLAGS) src/Tienda.cpp -o obj/Tienda.o
+	g++ $(FLAGS) tests/tests_tienda.cpp -o obj/tests_tienda.o
+	g++ $(FLAGS) src/Producto.cpp -o obj/Producto.o
+	g++ $(FLAGS) tests/tests_producto.cpp -o obj/tests_producto.o
+	g++ -g -o bin/tests obj/tests_tienda.o obj/Tienda.o obj/tests_producto.o obj/Producto.o -lgtest -lgtest_main -lpthread
 clean:
 	rm -Rf bin
--- tests/tests_producto.cpp
+++ tests/tests_producto.cpp
@@ -2,8 +2,6 @@
 #include "../src/Producto.h"
 
 /*
-    string obtenerInfoProducto();
-    int obtenerIdProducto();
     void modificarValores(int, string, int);
 
     friend ostream& operator << (ostream &o, const Producto*);
@@ -11,15 +9,45 @@
 
 namespace{
 
-    TEST(Test_Producto, Test_CrearProducto){
+    TEST(Test_Producto, Test_CrearProducto_ObtenerInfoProducto){
         /// AAA
 
         // Arrange - configurar el escenario
-        Producto producto(1, "Hamburguesas", 32);
+        Producto *producto= new Producto(1, "Hamburguesas", 32);
 
         // Act - ejecute la operación
-        string actual = producto.obtenerInfoProducto();
+        string actual = producto->obtenerInfoProducto();
         string esperada = "1 Hamburguesas 32";
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(Test_Producto, Test_ObtenerIdProducto){
+        /// AAA
+
+        // Arrange - configurar el escenario
+        Producto *producto =new Producto(23, "Hamburguesas", 32);
+
+        // Act - ejecute la operación
+        int actual = producto->obtenerIdProducto();
+        int esperada = 23;
+
+        // Assert - valide los resultados
+        EXPECT_EQ(esperada, actual);
+    }
+
+    TEST(Test_Producto, Test_ModificarValores){
+        /// AAA
+
+        // Arrange - configurar el escenario
+        Producto *producto =new Producto(23, "Hamburguesas", 32);
+
+        producto->modificarValores(43, "Papas", 444);
+
+        // Act - ejecute la operación
+        string actual = producto->obtenerInfoProducto();
+        string esperada = "43 Papas 444";
 
         // Assert - valide los resultados
         EXPECT_EQ(esperada, actual);
